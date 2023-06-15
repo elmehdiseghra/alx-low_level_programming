e95ce89821366ad752c8856194cb512f1b10e884
elmehdiseghra <elmehdiseghra@gmail.com>
2023-06-15 13:24:42 -0700

    first commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: a C program that prints with put function
+ *
+ * Return: always 0 (Success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle")
+	return (0);
+}
