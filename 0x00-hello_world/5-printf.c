7eb7c49e17737237ebe6c59fba23eeffd67ff593
elmehdiseghra <elmehdiseghra@gmail.com>
2023-06-15 13:39:55 -0700

    first commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: a C program that prints with put function
+ *
+ * Return: always 0 (Success)
+*/
+
+int main(void)
+{
+	puts("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
