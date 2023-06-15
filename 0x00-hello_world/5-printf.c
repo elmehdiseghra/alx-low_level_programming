0858b78be696441fb4da6241639e37a1fa75eb55
elmehdiseghra <elmehdiseghra@gmail.com>
2023-06-15 14:48:01 -0700

    first commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -7,9 +6,8 @@
  *
  * Return: always 0 (Success)
 */
-
 int main(void)
 {
-	puts("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
