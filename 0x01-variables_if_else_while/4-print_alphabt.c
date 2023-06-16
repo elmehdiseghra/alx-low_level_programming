b298396f39bd569e93f813e2675f1e55f6fae56c
elmehdiseghra <elmehdiseghra@gmail.com>
2023-06-16 11:35:32 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+ * main - entry point.
+ *
+ * Description: print all alphabet letters except q and e.
+ *
+ * Retun: Always 0 (Success).
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch == 'e' || ch == 'q')
+			ch++;
+		putchar(ch);
+		ch++
+	}
+	putchar('\n');
+	return (0);
+}
