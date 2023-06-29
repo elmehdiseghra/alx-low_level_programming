584e931e5c46c208e21f39512238248ebb021cd9
elmehdiseghra <elmehdiseghra@gmail.com>
2023-06-29 13:07:54 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -20,8 +20,8 @@ char *cap_string(char *str)
 		str[index - 1] == ';' ||
 		str[index - 1] == '.' ||
 		str[index - 1] == '!' ||
-		str[index - 1] == "?" ||
-		str[index - 1] == "'" ||
+		str[index - 1] == '?' ||
+		str[index - 1] == '"' ||
 		str[index - 1] == '(' ||
 		str[index - 1] == ')' ||
 		str[index - 1] == '{' ||
